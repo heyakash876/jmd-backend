db61fa6c22458115c12e60305e8951310464fcbc
heyakash876 <vermakash876@gmail.com>
2025-04-10 12:17:56 +0530

    port update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -29,8 +30,9 @@ mongoose.connect(process.env.MONGO_URI, {
   // Create Admin only once
   // createAdmin();
 
-  app.listen(5000, () => {
-    console.log('Server running on port 5000');
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }).catch(err => {
   console.error('MongoDB connection failed:', err);
